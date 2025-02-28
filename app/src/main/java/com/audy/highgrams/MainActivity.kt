fc42163b871645160daeca9375ac9341c8af5592
Rabi MARHOUM <rabi.marhoum@outlook.com>
2025-02-27 23:24:03 -0500

    add frangement commit

--- app/src/main/java/com/audy/highgrams/MainActivity.kt
+++ app/src/main/java/com/audy/highgrams/MainActivity.kt
@@ -1,18 +1,50 @@
 package com.audy.highgrams
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.audy.highgrams.Fragments.HomeFragment
+import com.audy.highgrams.Fragments.NotificationsFragment
+import com.audy.highgrams.Fragments.ProfileFragment
+import com.audy.highgrams.Fragments.SearchFragment
 import com.audy.highgrams.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.nav_home -> {
+                moveToFragment(HomeFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_search -> {
+                moveToFragment(SearchFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_add_post -> {
+                item.isChecked = false
+                startActivity(Intent(this@MainActivity, AddPostActivity::class.java))
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_notifications -> {
+                moveToFragment(NotificationsFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_profile -> {
+                moveToFragment(ProfileFragment())
+                return@OnNavigationItemSelectedListener true
+            }
+        }
 
+        false
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -21,15 +53,26 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_view)
+        val navController = findNavController(R.id.main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_notifications, R.id.nav_search
+                R.id.nav_home, R.id.nav_search, R.id.nav_notifications,  R.id.nav_profile, R.id.nav_search,
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        moveToFragment(HomeFragment())
+    }
+
+    private fun moveToFragment(fragment: Fragment)
+    {
+        val fragmentTrans = supportFragmentManager.beginTransaction()
+        fragmentTrans.replace(R.id.fragment_container, fragment)
+        fragmentTrans.commit()
     }
 }
