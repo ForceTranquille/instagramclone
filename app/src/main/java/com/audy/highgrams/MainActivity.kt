8abfc590c1e2ecda6f4ed658c31c098b637f4a2d
Rabi MARHOUM <rabi.marhoum@outlook.com>
2025-03-01 18:54:27 -0500

    add username  commit

--- app/src/main/java/com/audy/highgrams/MainActivity.kt
+++ app/src/main/java/com/audy/highgrams/MainActivity.kt
@@ -1,47 +1,57 @@
 package com.audy.highgrams
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.replace
+import com.audy.highgrams.databinding.ActivityMainBinding
 import com.audy.highgrams.fragments.HomeFragment
 import com.audy.highgrams.fragments.NotificationsFragment
 import com.audy.highgrams.fragments.ProfileFragment
 import com.audy.highgrams.fragments.SearchFragment
-import com.audy.highgrams.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    internal var selectedFragment: Fragment? = null
 
     // Listener for bottom navigation item selections
-    private val onNavigationItemSelectedListener = NavigationBarView.OnItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.nav_home -> {
-                moveToFragment(HomeFragment())
-                return@OnItemSelectedListener true
+    private val onNavigationItemSelectedListener =
+        NavigationBarView.OnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    selectedFragment = HomeFragment()
+
+                }
+
+                R.id.nav_search -> {
+                    selectedFragment = SearchFragment()
+
+                }
+
+                R.id.nav_add_post -> {
+                    return@OnItemSelectedListener true
+                }
+
+                R.id.nav_notifications -> {
+                    selectedFragment = NotificationsFragment()
+                }
+
+                R.id.nav_profile -> {
+                    selectedFragment = ProfileFragment()
+                }
             }
-            R.id.nav_search -> {
-                moveToFragment(SearchFragment())
-                return@OnItemSelectedListener true
+            if (selectedFragment != null)
+            {
+                supportFragmentManager.beginTransaction().replace(
+                    R.id.fragment_container,
+                    selectedFragment!!
+                ).commit()
             }
-            R.id.nav_add_post -> {
-                // Handle the add post action here, for example, open a dialog or a new activity
-                item.isChecked = false
-                return@OnItemSelectedListener true
-            }
-            R.id.nav_notifications -> {
-                moveToFragment(NotificationsFragment())
-                return@OnItemSelectedListener true
-            }
-            R.id.nav_profile -> {
-                moveToFragment(ProfileFragment())
-                return@OnItemSelectedListener true
-            }
+            false
         }
-        false
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
